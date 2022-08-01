b5906b8e07d40c837f45a2455e800c04f09ecf38
keisuke-yanagisawa <rayofmoonlightk@gmail.com>
2022-08-01 05:01:30 +0000

    update

--- automatic_docking.py
+++ automatic_docking.py
@@ -1,7 +1,10 @@
 import argparse
 import os
+from dataclasses import dataclass
+from typing import List
 
 from scripts.utilities import util
+from scripts.utilities.util import dump_enter_exit_on_debug_log
 from scripts.utilities.logger import logger
 from scripts.executables import autodock
 
@@ -20,13 +23,19 @@ except ImportError:
 
 VERSION = "0.1.0"
 
-def get_current_function_name():
-  import inspect
-  cur_frame = inspect.currentframe()
-  return cur_frame.f_back.f_code.co_name
+@dataclass
+class BindingSite:
+  center: List[float]
+  width: List[float]
 
-def predict_binding_site(proteinfile, outputdir="output"):
-  logger.info(f"start: {get_current_function_name()}")
+@dump_enter_exit_on_debug_log
+def predict_binding_site(proteinfile: str, margin: float=5) -> List[BindingSite]:
+  """
+  3D-CNNによる化合物結合部位予測手法 Kalasanty に基づいて
+  結合部位を列挙する。
+  """
+  if not hasattr(margin, "__iter__"):
+    margin = [margin, margin, margin]
 
   model = UNet.load_model(
     "/kalasanty/data/model_scpdb2017.hdf",
@@ -38,51 +47,67 @@ def predict_binding_site(proteinfile, outputdir="output"):
   mol_protein = next(pybel.readfile(protein_ext, proteinfile))
 
   density, origin, step = model.pocket_density_from_mol(mol_protein)
-  print(density)
-  pockets = model.get_pockets_segmentation(density, threshold=1e-10, min_size=1)
+  # print(density)
+  pocket_indices = model.get_pockets_segmentation(density)
   import numpy as np
-  numbers = set(list(pockets.reshape(-1)))
-  for num in numbers:
-    if num == 0:
-      continue
-    # print(pockets.shape, step, origin)
-    # xbin = np.arange(pockets.shape[0]) * step[0] + origin[0]
-    # ybin = np.arange(pockets.shape[1]) * step[1] + origin[1]
-    # zbin = np.arange(pockets.shape[2]) * step[2] + origin[2]
-    # g = Grid((pockets==num).astype(int), [xbin, ybin, zbin])
-    # g.export(f"test{num}.dx")
 
-    #これでいい感じなので、各pocketを完全に内包するようなboxを作成して計算を回す
-    # でも…thresholdとmin_sizeに対して、緩くしすぎると却ってpocket数が減るのはイメージができない
-  # print(pockets)
-  # print(np.sum(pockets))
-  # os.system(f"""
-  #   python $KALASANTY_ROOT/kalasanty/scripts/predict.py \
-  #   -f {protein_ext} -i {proteinfile} --output {outputdir}
-  # """)
-  # protein_dir = os.path.dirname(proteinfile)
-  # os.system(f"head {outputdir}/{protein_dir}/pocket0.mol2")
+  candidate_indices = set(list(pocket_indices.reshape(-1)))
+  candidate_indices.remove(0)
 
-  logger.info(f"end: {get_current_function_name()}")
+  binding_sites = []
 
+  for c_idx in candidate_indices:
+    xbin = np.arange(pocket_indices.shape[0]) * step[0] + origin[0]
+    ybin = np.arange(pocket_indices.shape[1]) * step[1] + origin[1]
+    zbin = np.arange(pocket_indices.shape[2]) * step[2] + origin[2]
+    g = Grid((pocket_indices==c_idx).astype(int), [xbin, ybin, zbin])
+    g.export(f"test{c_idx}.dx")
+    x_indices, y_indices, z_indices = np.where(pocket_indices==c_idx)
+    x_min, x_max = xbin[min(x_indices)], xbin[max(x_indices)]
+    y_min, y_max = ybin[min(y_indices)], ybin[max(y_indices)]
+    z_min, z_max = zbin[min(z_indices)], zbin[max(z_indices)]
+    print(x_min, x_max, y_min, y_max, z_min, z_max)
+    x_center = (x_min + x_max) / 2
+    y_center = (y_min + y_max) / 2
+    z_center = (z_min + z_max) / 2
+    x_width = x_max - x_min + margin[0]
+    y_width = y_max - y_min + margin[1]
+    z_width = z_max - z_min + margin[2]
+    binding_sites.append(BindingSite([x_center, y_center, z_center], [x_width, y_width, z_width]))
+
+  return binding_sites
+
+
+@dump_enter_exit_on_debug_log
 def prepare_protein(proteinfile):
-  logger.info(f"start: {get_current_function_name()}")
+  """
+  ドッキング計算前のタンパク質処理
+  #TODO: ドッキングツールによって事前処理は異なるはず。
+         ドッキングツールに応じたクラスのメソッドにしたほうがよいか。
+  """
   prep = autodock.PrepareReceptor()
   prep.set_receptor(proteinfile)
   pdbqtfile = prep.run()
-  logger.info(f"end: {get_current_function_name()}")
   return pdbqtfile
 
+@dump_enter_exit_on_debug_log
 def rdmol2obmol(rdmol):
+  """
+  RDKitのMoleculeオブジェクトをOpenBabelのMoleculeオブジェクトに変換する
+  """
   tmpfile=".tmp.sdf"
   print(Chem.MolToMolBlock(rdmol), file=open(tmpfile, "w"))
   mol = next(pybel.readfile("sdf", tmpfile))
   os.remove(tmpfile)
   return mol
 
+@dump_enter_exit_on_debug_log
 def prepare_ligand(ligandfile):
+  """
+  ドッキング計算前の化合物処理
+  #TODO: ドッキングツールによって事前処理は異なるはず。
+  """
   tmpfile=".tmp.mol2"
-  logger.info(f"start: {get_current_function_name()}")
   smiles = open(ligandfile).read().strip()
   rdmol = Chem.MolFromSmiles(smiles)
   rdmol = Chem.AddHs(rdmol)
@@ -99,32 +124,65 @@ def prepare_ligand(ligandfile):
   prep = autodock.PrepareLigand()
   prep.set_ligand(tmpfile)
   pdbqtfile = prep.run()
-  logger.info(f"end: {get_current_function_name()}")
   os.remove(tmpfile)
   return [pdbqtfile]
 
-def exec_docking(proteinfile, ligandfiles):
-  # TODO 複数構造対応
-  ligandfile = ligandfiles[0]
 
-  logger.info(f"start: {get_current_function_name()}")
+@dump_enter_exit_on_debug_log
+def exec_docking(proteinfile: str, ligandfiles: List[str], 
+                 binding_sites: List[BindingSite],
+                 exhaustiveness: int=1, n_poses: int=100):
+  """
+  AutoDock Vinaを用いてドッキング計算を行う。
+  ここで入力するcenter, box_sizeの単位はいずれもAngstromである。
+
+  MEMO: 
+  通常exhaustivenessは8であるが、
+  計算時間と計算精度のトレードオフを考えると1のほうが良い、
+  ということがREstretto論文執筆時に判明したため
+  それを初期設定としている。
+  """
+
+  # TODO 複数構造対応
+  if hasattr(ligandfiles, "__iter__"):
+    if len(ligandfiles) > 1:
+      logger.warning("multiple ligands are not supported yet. use the first one.")
+    ligandfile = ligandfiles[0]
+
   from vina import Vina
   v = Vina(seed=42)
   v.set_receptor(proteinfile)
   v.set_ligand_from_file(ligandfile)
-  v.compute_vina_maps(center=[0,80,110], box_size=[30,30,30])
-  v.dock(exhaustiveness=1, n_poses=100)
-  v.optimize()
-  logger.info(f"end: {get_current_function_name()}")
+
+  # TODO 複数箇所のドッキングに対応させる
+  binding_sites = binding_sites[0:1]
+  for site in binding_sites:
+    v.compute_vina_maps(center=site.center, box_size=site.width)
+    v.dock(exhaustiveness=exhaustiveness, n_poses=n_poses)
+    v.optimize()
+
   return v
 
+@dump_enter_exit_on_debug_log
 def output_poses(vina_obj, outputfile):
-  tmpfile = ".tmp.pdbqt"
-  vina_obj.write_poses(tmpfile, overwrite=True, n_poses=100)
+  """
+  AutoDock Vinaの計算結果を任意の形式に変換して出力する。
+  AutoDock Vinaの標準的な入出力はpdbqtファイルだが、
+  あまり一般的なフォーマットではないので、異なる形式に変更することを前提としている。
+  # TODO: exec_docking()以外にドッキングツールに依存する場所があるべきではない。
+          あるいは、ドッキングツールはすべて同一のインタフェースを持つclassで書き起こされるべき。
+          （こちらのほうが適切に思われる）
+  """
 
   ext = os.path.splitext(os.path.basename(outputfile))[-1][1:]
+  if ext == "pdbqt": # pdbqt形式の場合は特別な処理を行わなくて良い
+    vina_obj.write_poses(outputfile, overwrite=True, n_poses=100)
+    return
+
+  # pybelを通して各種ファイルフォーマットに変換する
+  pdbqtfile = ".tmp.pdbqt"
   writer = pybel.Outputfile(ext, outputfile, overwrite=True)
-  for mol in pybel.readfile("pdbqt", tmpfile):
+  for mol in pybel.readfile("pdbqt", pdbqtfile):
     writer.write(mol)
   writer.close()
 
@@ -149,7 +207,7 @@ if __name__ == "__main__":
   sites   = predict_binding_site(args.protein)
   protein = prepare_protein(args.protein)
   ligands = prepare_ligand(args.ligand)
-  obj     = exec_docking(protein, ligands)
+  obj     = exec_docking(protein, ligands, sites)
   output_poses(obj, args.output)
 
   
--- scripts/utilities/util.py
+++ scripts/utilities/util.py
@@ -1,11 +1,23 @@
+from .logger import logger
 import os
 
-def getabsolutepath(path):
+def getabsolutepath(path: str) -> str:
   path = expandpath(path)
   if not path.startswith("/"): # relative path
     path = os.getcwd() + "/" + path
   return path
 
-def expandpath(path):
+def expandpath(path: str) -> str:
   path = os.path.expanduser(path)
   return os.path.expandvars(path)
+
+def dump_enter_exit_on_debug_log(func):
+  """
+  関数の開始・終了をdebugログに出力するための関数デコレーター
+  """
+  def wrapper(*args, **kwargs):
+    logger.debug(f"start: {func.__name__}")
+    ret = func(*args, **kwargs)
+    logger.debug(f"end: {func.__name__}")
+    return ret
+  return wrapper
